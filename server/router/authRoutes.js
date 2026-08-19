5d94afd5811d790e9309e3ff8ea7fef36b91bebe
Asif Shahriar <cse12305029brur@gmail.com>
2026-08-20 05:21:02 +0600

    2nd commt

---
+++ server/router/authRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  register,
+  getUser,
+  login,
+  logout,
+} from "../controllers/authController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", isAuthenticated, getUser);
+router.get("/logout", isAuthenticated, logout);
+ 
+
+export default router;
