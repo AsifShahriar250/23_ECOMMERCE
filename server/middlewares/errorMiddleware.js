54a53842c0482c85c6707c4c9556c37730b18687
aaa <aaa@gmail.com>
2026-08-20 05:21:02 +0600

    2nd commt

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -7,13 +7,13 @@ class ErrorHandler extends Error {
 
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
-  err.statusCode = statusCode || 500;
+  err.statusCode = err.statusCode || 500;
   if (err.code === 11000) {
     const message = `Duplicate field value entered`;
     err = new ErrorHandler(message, 400);
   }
 
-  if ((err.name = "JsonWebTokenError")) {
+  if (err.name === "JsonWebTokenError") {
     const message = "JSON Web Token is invalid, try again";
     err = new ErrorHandler(message, 400);
   }
@@ -22,18 +22,17 @@ export const errorMiddleware = (err, req, res, next) => {
     const message = "JSON Web Token is invalid, try again";
     err = new ErrorHandler(message, 400);
   }
-  
-  const errorMessage = err.error
+
+  const errorMessage = err.errors
     ? Object.values(err.errors)
         .map((error) => error.message)
         .join(" ")
     : err.message;
 
-    return res.statusCode(err.statusCode).json({
-      success: false,
-      message: errorMessage,
-    });
+  return res.status(err.statusCode).json({
+    success: false,
+    message: errorMessage,
+  });
 };
 
 export default ErrorHandler;
-
