e2708dd69c64fa662b8088dab2cf17fa01b1b14b
Asif Shahriar <cse12305029brur@gmail.com>
2026-08-23 01:09:00 +0600

    product controller add

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -6,6 +6,8 @@ import {
   logout,
   forgotPassword,
   resetPassword,
+  updatePassword,
+  updateProfile,
 } from "../controllers/authController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
@@ -14,8 +16,10 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", isAuthenticated, getUser);
-router.get("/logout",isAuthenticated, logout);
+router.get("/logout", isAuthenticated, logout);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
+router.put("/password/update", isAuthenticated, updatePassword);
+router.put("/profile/update", isAuthenticated, updateProfile);
 
 export default router;
